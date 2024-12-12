d391057169e87be580e86d7ff3a1847222c70791
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-12-12 13:10:48 +0000

    [Add]color_pickerのjs

--- color_picker/js/script.js
+++ color_picker/js/script.js
@@ -1,1 +1,22 @@
-console.log('JavaScript読み込みテスト')
+// 定数を宣言
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+
+// 関数を定義
+
+const colorBg = () => {
+    // バックティック(``)で囲むと${}がつかえる（テンプレート文字列）
+    // バックティックはshift+@
+    text.textContent = `カラーコード：${color.value}`;
+};
+
+
+// 何が.addEventListner(どうなったら,どうなる)
+// addEventListnerでイベントを設定
+
+// 何が＝color
+// どうなったら（イベント）＝入力（input）されたら
+// 
+
+color.addEventListener('input',colorBg);
