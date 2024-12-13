5fa5240bc998b862e55e86cb34afe67a05a5d5ac
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-12-13 12:48:14 +0000

    [finish]color_pickerの完成

--- color_picker/js/script.js
+++ color_picker/js/script.js
@@ -6,9 +6,21 @@ const color = document.querySelector('#colorPicker');
 // 関数を定義
 
 const colorBg = () => {
+    // 選択した色を背景色に指定
+    document.body.style.backgroundColor = color.value;
+    // html.bodyタグ.style.CSSプロパティ = 値;
+    
+    // カラーコードを表示
     // バックティック(``)で囲むと${}がつかえる（テンプレート文字列）
     // バックティックはshift+@
-    text.textContent = `カラーコード：${color.value}`;
+    if(color.value === '#ffffff'){
+        text.textContent = `カラーコード：${color.value}(white)`;
+    }else if(color.value === '#000000'){
+        text.textContent = `カラーコード：${color.value}(black)`;
+    }else{
+        text.textContent = `カラーコード：${color.value}`;
+    }
+    
 };
 
 
@@ -17,6 +29,6 @@ const colorBg = () => {
 
 // 何が＝color
 // どうなったら（イベント）＝入力（input）されたら
-// 
+// 関数colorBgを発動
 
 color.addEventListener('input',colorBg);
