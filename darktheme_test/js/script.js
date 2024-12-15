12a94c865a9e91e254536198af1f83ba13802a13
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-12-15 12:07:04 +0000

    [Finish]CSS文字列カウント

--- darktheme_test/js/script.js
+++ darktheme_test/js/script.js
@@ -1,11 +1,17 @@
 const button = document.querySelector('#btn');
+const text = document.querySelector('#text');
+const count = document.querySelector('#count');
 
-btn.addEventListener("click", () => {
+btn.addEventListener('click', () => {
     document.body.classList.toggle('darktheme');
     
     if (btn.textContent === 'ダークモードに変更'){
-        btn.textContent = 'ライトモードに変更'
+        btn.textContent = 'ライトモードに変更';
     } else {
-        btn.textContent = 'ダークモードに変更'
+        btn.textContent = 'ダークモードに変更';
     }
-})
+});
+
+text.addEventListener('keyup', () => {
+    count.textContent = text.value.length;
+});
