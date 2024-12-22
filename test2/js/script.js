47e69078f686c5d74bfbca2343dffc1b1f7ae0b2
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-12-22 13:15:33 +0000

    スクロールボタン練習

--- test2/js/script.js
+++ test2/js/script.js
@@ -1,9 +1,18 @@
 const btn = document.querySelector('#btn');
+const sc_btn = document.querySelector('#sc_btn');
 const message = document.querySelector('#message');
 
-// textにイベント付与
+// btnにイベント付与
 btn.addEventListener('click',() => {
     message.textContent = `スイッチON`
     btn.classList.toggle('green');
     message.classList.toggle('hidden')
 });
+
+// 上までスクロールするボタン
+sc_btn.addEventListener('click',() => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth"
+    });
+})
