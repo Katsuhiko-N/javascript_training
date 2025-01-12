318952050c1fd1c907e3a12b06b87046bf5fe961
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2025-01-12 13:23:31 +0000

    オブジェクトの分割代入を確認

--- pictures_index/js/script.js
+++ pictures_index/js/script.js
@@ -2,21 +2,43 @@ const menu = document.querySelector('#menu');
 
 // ファイル名の配列を作成
 const pictures = [
-    'sample-A',
-    'sample-B',
-    'sample-C',
-    'sample-D',
-    'sample-E',
-    'sample-F'
+    {
+        name:'sample-A',
+        color:'赤'
+    },
+    {
+        name:'sample-B',
+        color:'黄'
+    },
+    {
+        name:'sample-C',
+        color:'青'
+    },
+    {
+        name:'sample-D',
+        color:'紫'
+    },
+    {
+        name:'sample-E',
+        color:'茶'
+    },
+    {
+        name:'sample-F',
+        color:'灰'
+    }
     ];
 
 console.log(pictures.length)
 
 // 配列の長さの文繰り返し
 for(let i = 0; i < pictures.length; i++){
+    // 分割代入
+    const{name,color} = pictures[i];
+    
     // html文を定数に代入
-    const content = `<div><image src="images/${pictures[i]}.jpg" alt=""></div>`;
+    const content = `<div><image src="images/${name}.jpg" alt=""><p>${color}</p></div>`;
     
     // insertAdjacentHTMLでhtmlとして挿入
     menu.insertAdjacentHTML('beforeend',content);
+    
 }
